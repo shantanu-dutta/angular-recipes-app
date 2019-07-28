0f905ef67bc3ae7c2c1d4fc6ebd859945783482b
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-07-28 20:58:57 +0530

    1.  Refactored Recipe model. 2.  Added Ingredient model.

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,3 @@
-export class Recipe { 
-  public name: string;
-  public description: string;
-  public imagePath: string;
-
-  constructor(name: string, description: string, imagePath: string) {
-    this.name = name;
-    this.description = description;
-    this.imagePath = imagePath;
-  }
+export class Recipe {
+  constructor(public name: string, public description: string, public imagePath: string) { }
 }
