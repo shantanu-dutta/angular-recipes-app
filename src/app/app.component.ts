2bf0daffd4581727022f03e6dcc3acc8a1e2b614
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-07-29 10:13:07 +0530

    Changed pages on header link click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeature = 'recipes';
+
+  onNavigate(feature: string): void { this.loadedFeature = feature; }
+  showRecipes(): boolean { return this.loadedFeature === 'recipes'; }
+  showShoppingList(): boolean { return this.loadedFeature === 'shopping-list'; }
+}
