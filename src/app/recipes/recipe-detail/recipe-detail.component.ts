3ade0bc095f000f5f41b7310d88a4be8a23e8ec8
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-07-31 08:51:23 +0530

    Implemented functionality for adding recipe Ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, Input,  OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
+import { RecipeService } from '../recipe.service';
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -10,9 +12,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    this.recipeService.AddIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,43 @@
-import { EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+
+import { ShoppingListService } from '../shopping-list/Shopping-list.service';
+
+@Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://burgerscoici.ro/wp-content/uploads/2018/10/sardina2_640-x-640-px.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'http://burgerscoici.ro/wp-content/uploads/2018/10/sardina2_640-x-640-px.jpg'),
+    new Recipe(
+      'Tasty Schnitzel',
+      'Super tasty Schnitzel - just awesome!',
+      'http://burgerscoici.ro/wp-content/uploads/2018/10/sardina2_640-x-640-px.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'http://burgerscoici.ro/wp-content/uploads/2018/10/sardina2_640-x-640-px.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ]
+    ),
   ];
-
   private recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes(): Recipe[] { return this.recipes.slice(); }
 
   getSelectedRecipe() { return this.recipeSelected.asObservable(); }
 
   selectRecipe(recipe: Recipe): void { this.recipeSelected.emit(recipe); }
+
+  AddIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.AddIngredients(ingredients);
+  }
 }
