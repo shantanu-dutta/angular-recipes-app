2bcd693e010e87df33335b45e8e131e372d7861e
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-16 12:55:25 +0530

    1. Redirected after successful signin. 2. Generated empty auth guard service.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
+@Injectable()
 export class AuthService {
   private token: string;
+
+  constructor(private router: Router) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -13,6 +18,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
               (token: string) => this.token = token
