de36c104fc916f23c7448ed10614326af68f38e4
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-16 21:19:20 +0530

    1. Created Logging interceptor to log response. 2. Added Logging interceptor in core module providers.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -15,6 +15,7 @@ import { RecipeService } from '../recipes/recipe.service';
 import { ShoppingListService } from '../shopping-list/Shopping-list.service';
 
 import { AuthInterceptor } from '../shared/auth.interceptor';
+import { LoggingInterceptor } from '../shared/logging.interceptor';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { AuthInterceptor } from '../shared/auth.interceptor';
     RecipeService,
     ShoppingListService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true, },
+    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true, },
   ],
 })
 export class CoreModule { }
