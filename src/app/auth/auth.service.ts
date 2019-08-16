6f3f5dab67e12c01fbb593ddcdf2ab3dfda373d7
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-16 12:16:23 +0530

    Imported only required firebase modules.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 export class AuthService {
   signupUser(email: string, password: string) {
