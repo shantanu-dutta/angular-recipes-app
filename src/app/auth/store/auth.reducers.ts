e3602509dbe76400edfac0d32ee61207321108e5
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-15 21:12:54 +0530

    1. Added reducers for auth actions.

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -1,4 +1,6 @@
-import { Action, createReducer } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
+
+import * as AuthActions from './auth.actions';
 
 export interface State {
   token: string;
@@ -10,7 +12,11 @@ const initialState: State = {
   autheticated: false
 };
 
-const authReducer = createReducer(initialState);
+const authReducer = createReducer(
+  initialState,
+  on(AuthActions.signup, AuthActions.signin, state => ({ ...state, autheticated: true })),
+  on(AuthActions.logout, state => ({ ...state, token: null, autheticated: false }))
+);
 
 export function reducer(state: State | undefined, action: Action) {
   return authReducer(state, action);
