988f448382abedbb87973c2ed61cef8ba55ebf21
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-16 10:17:35 +0530

    1. Replaced authentication check in header component by store auth state.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Store } from '@ngrx/store';
+
+import { Observable } from 'rxjs';
 
 import { AuthService } from '../../auth/auth.service';
 import { DataStorageService } from '../../shared/data-storage.service';
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
 
 @Component({
   selector: 'app-header',
@@ -17,13 +22,20 @@ import { DataStorageService } from '../../shared/data-storage.service';
     ])
   ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   collapse = 'closed';
+  authState$: Observable<fromAuth.State>;
+
   constructor(
     public authService: AuthService,
     private dataStorageService: DataStorageService,
-    private router: Router
+    private router: Router,
+    private store: Store<fromApp.AppState>
   ) {}
+
+  ngOnInit() {
+    this.authState$ = this.store.select('auth');
+  }
 
   toggleCollapse() {
     this.collapse = this.collapse === 'open' ? 'closed' : 'open';
