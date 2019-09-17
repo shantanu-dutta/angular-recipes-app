5666ba257d74b3c1123654ae8a4f276bca565047
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-17 11:05:46 +0530

    1. Added @ngrx/effects package. 2. Created auth.effects file. 3. Added EffectsModule to app module imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
@@ -11,6 +12,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AppComponent } from './app.component';
 
 import { reducers } from './store/app.reducers';
+import { AuthEffects } from './auth/store/auth.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +23,8 @@ import { reducers } from './store/app.reducers';
     CoreModule,
     HttpClientModule,
     ShoppingListModule,
-    StoreModule.forRoot(reducers)
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AuthEffects])
   ],
 
   bootstrap: [AppComponent]
