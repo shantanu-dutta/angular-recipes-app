e2ef26d5733a342a0ace35f2a3dcc2f987bacf59
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-30 08:46:17 +0530

    Added recipe reducer functions.

--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,6 +1,7 @@
-import { Action, createReducer } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 
 import { Recipe } from '../recipe.model';
+import * as RecipeActions from './recipe.actions';
 
 export const recipesFeatureKey = 'recipes';
 
@@ -12,7 +13,26 @@ const initialState: State = {
   recipes: []
 };
 
-const recipesReducer = createReducer(initialState);
+const recipesReducer = createReducer(
+  initialState,
+  on(RecipeActions.addRecipe, (state, { recipe }) => ({
+    ...state,
+    recipes: [...state.recipes, recipe]
+  })),
+  on(RecipeActions.updateRecipe, (state, { index, updatedRecipe }) => {
+    const oldRecipe = state.recipes[index];
+    const modifiedRecipe = { ...oldRecipe, ...updatedRecipe };
+    const recipes = [...state.recipes];
+    recipes[index] = modifiedRecipe;
+    return { ...state, recipes };
+  }),
+  on(RecipeActions.deleteRecipe, (state, { index }) => {
+    const oldRecipes = [...state.recipes];
+    const newRecipes = oldRecipes.splice(index, 1);
+    return { ...state, recipes: newRecipes };
+  }),
+  on(RecipeActions.setRecipes, (state, { recipes }) => ({ ...state, recipes: [...recipes] }))
+);
 
 export function reducer(state: State | undefined, action: Action) {
   return recipesReducer(state, action);
