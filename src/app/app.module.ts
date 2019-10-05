a2aaf2586b4361d1f94ad77afc5f0aecbcddc4b5
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-10-05 10:54:51 +0530

    Generated angular universal packages using @nguniversal/express-engine schematics.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { AuthEffects } from './auth/store/auth.effects';
   declarations: [AppComponent],
   imports: [
     AuthModule,
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     CoreModule,
     HttpClientModule,
